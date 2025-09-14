430f1a9975cc8c5c1ed4c6b7258d7619eb2cbf70
digu-d <diguguido13@gmail.com>
2025-09-14 09:52:27 -0300

    Create essay2.py
    outra versão de um código utilizado no curso

---
+++ essay2.py
@@ -0,0 +1,17 @@
+idade = 15
+age = "minha idade"
+print ("minha idade é " + str(idade))
+
+
+minhaAltura =  str(1.75)
+idade += float(minhaAltura)
+
+
+#print("minha altura " + str(minhaAltura) + " somada minha idade é " + str(idade) )
+
+
+idadeBol = False
+minhaAlturaBol = True
+
+
+#print("minha altura " + minhaAltura + " está  " + str(minhaAlturaBol) + " e minha idade " + str(idade) + " está " + str(idadeBol)  )
