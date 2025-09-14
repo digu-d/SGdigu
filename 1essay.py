6e477c7ba2447a4b8f3ec6f63a2077cbdcbe8276
digu-d <diguguido13@gmail.com>
2025-09-14 09:51:31 -0300

    Create 1essay.py
    umas das versoes de um codigo utilizado no curso

---
+++ 1essay.py
@@ -0,0 +1,15 @@
+Atividade 1 
+
+primeiraParte = "Hello  World"
+segundaParte = "diogo"
+print ("oi " + primeiraParte)
+print ("ola " + segundaParte)
+variavel1 = "10"
+variavel2 = "5"
+print ("oi " + variavel1)
+print ("oi " +  variavel2)
+variavel3 = 10 % 5
+print ("oi ")
+print (variavel3)
+
+
